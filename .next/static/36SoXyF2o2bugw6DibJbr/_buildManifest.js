669ed4c754e5a9b8b459fe1f7de5ccc7478b3f0c
ADM Jader dos Santos Morais <admjsmorais@tj.ba.gov.br>
2021-03-04 00:04:58 -0300

    teste1

---
+++ .next/static/36SoXyF2o2bugw6DibJbr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-1c3aa1d8abc1e9872fd6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f6f18b847a52f44b3089.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ .next/static/yzb58Ae09cIid28BjohIu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-649591a405dea83beb60.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f6f18b847a52f44b3089.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
